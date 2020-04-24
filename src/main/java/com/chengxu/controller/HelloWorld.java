e7f72c0e31e9b6fd767b73964f0bbd17ae1dfb1a
chengxu <chengxusir@gmail.com>
2020-04-24 16:35:20 +0800

    333

--- src/main/java/com/chengxu/controller/HelloWorld.java
+++ src/main/java/com/chengxu/controller/HelloWorld.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 	@GetMapping("/hello")
 	public String doReq() {
-		return "hello,world! 我的jenkins部署在内网,所以GitHub无法访问,webhooks是不好用的" + "只能在浏览器地址栏试试啦！！！"
+		return "hello,Jenkins! 我的jenkins部署在内网,所以GitHub无法访问,webhooks是不好用的" + "只能在浏览器地址栏试试啦！！！"
 				+ "http://192.168.5.5:8080/jenkins/job/TestGit/build?token=GIT_TOKEN   </br></br></br> [请求时间: "
 				+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "]";
 	}
