05205188a54a1d3e9278e4a21a16dbabe136b57f
chengxu <chengxusir@gmail.com>
2020-04-24 14:34:47 +0800

    aaa

--- src/main/java/com/chengxu/controller/HelloWorld.java
+++ src/main/java/com/chengxu/controller/HelloWorld.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 	@GetMapping("/hello")
 	public String doReq() {
-		return "hello,world! 最后一次提交4444444444444！！！   </br></br></br> [请求时间: "
+		return "hello,world! 最后一次提交777777777！！！   </br></br></br> [请求时间: "
 				+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "]";
 	}
 }
