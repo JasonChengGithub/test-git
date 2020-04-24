7f39f0c93b608a74bf555326f30c8f0d1cd6e750
chengxu <chengxusir@gmail.com>
2020-04-24 16:05:35 +0800

    1111

--- src/main/java/com/chengxu/controller/HelloWorld.java
+++ src/main/java/com/chengxu/controller/HelloWorld.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 	@GetMapping("/hello")
 	public String doReq() {
-		return "hello,world! 最后一次提交123456！！！   </br></br></br> [请求时间: "
+		return "hello,world! 我的jenkins部署在内网,所以GitHub无法访问,webhooks是不好用的" + "只能在浏览器地址栏试试啦！！！   </br></br></br> [请求时间: "
 				+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "]";
 	}
 }
